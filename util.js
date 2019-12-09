8b45a4f3fb38f10cfc1e82c10ba81e9681f1296b
Daniel <mwdchang@gmail.com>
2019-12-08 22:04:41 -0500

    billboard

---
+++ util.js
@@ -0,0 +1,44 @@
+/**
+ * See https://threejsfundamentals.org/threejs/lessons/threejs-billboards.html
+ */
+function makeLabelCanvasTexture(baseWidth, size, name) {
+    const borderSize = 2;
+    const ctx = document.createElement('canvas').getContext('2d');
+    const font =  `${size}px Tahoma`;
+    ctx.font = font;
+    // measure how long the name will be
+    const textWidth = ctx.measureText(name).width;
+
+    const doubleBorderSize = borderSize * 2;
+    const width = baseWidth + doubleBorderSize;
+    const height = size + doubleBorderSize;
+    ctx.canvas.width = width;
+    ctx.canvas.height = height;
+
+    console.log(ctx.canvas.width, ctx.canvas.height);
+
+    // need to set font again after resizing canvas
+    ctx.font = font;
+    ctx.textBaseline = 'middle';
+    ctx.textAlign = 'center';
+
+    ctx.fillStyle = 'transparent';
+    ctx.fillRect(0, 0, width, height);
+
+    // scale to fit but don't stretch
+    const scaleFactor = Math.min(1, baseWidth / textWidth);
+    ctx.translate(width / 2, height / 2);
+    ctx.scale(scaleFactor, 1);
+    ctx.fillStyle = 'white';
+    ctx.fillText(name, 0, 0);
+
+    // return ctx.canvas;
+
+    // create texture
+    const texture = new THREE.CanvasTexture(ctx.canvas);
+    texture.minFilter = THREE.LinearFilter;
+    texture.wrapS = THREE.ClampToEdgeWrapping;
+    texture.wrapT = THREE.ClampToEdgeWrapping;
+
+    return texture;
+  }
